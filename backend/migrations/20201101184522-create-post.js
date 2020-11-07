f51a4b0af846af115200438f1af27de4bca42b9f
Thomas Claireau <thomas.claireau@gmail.com>
2020-11-07 09:46:34 +0100

    update migration post

--- backend/migrations/20201101184522-create-post.js
+++ backend/migrations/20201101184522-create-post.js
@@ -40,64 +40,8 @@ module.exports = {
 				type: Sequelize.DATE,
 			},
 		});
-
-		await queryInterface.createTable('posts__likes', {
-			id: {
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-				type: Sequelize.INTEGER,
-			},
-			postId: {
-				allowNull: false,
-				type: Sequelize.INTEGER,
-				references: {
-					model: 'Posts',
-					key: 'id',
-				},
-				onUpdate: 'cascade',
-				onDelete: 'cascade',
-			},
-			userId: {
-				allowNull: false,
-				type: Sequelize.INTEGER,
-				references: {
-					model: 'Users',
-					key: 'id',
-				},
-				onUpdate: 'cascade',
-				onDelete: 'cascade',
-			},
-		});
-
-		await queryInterface.createTable('posts__dislikes', {
-			id: {
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-				type: Sequelize.INTEGER,
-			},
-			postId: {
-				allowNull: false,
-				type: Sequelize.INTEGER,
-				references: {
-					model: 'Posts',
-					key: 'id',
-				},
-			},
-			userId: {
-				allowNull: false,
-				type: Sequelize.INTEGER,
-				references: {
-					model: 'Users',
-					key: 'id',
-				},
-			},
-		});
 	},
 	down: async (queryInterface, Sequelize) => {
-		await queryInterface.dropTable('posts__likes');
-		await queryInterface.dropTable('posts__dislikes');
 		await queryInterface.dropTable('Posts');
 	},
 };
