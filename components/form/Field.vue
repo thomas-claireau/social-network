825c04cddb3e8605f7201f36a937b76a99816b61
Thomas Claireau <thomas.claireau@gmail.com>
2020-11-13 07:44:36 +0100

    field info OK

--- components/form/Field.vue
+++ components/form/Field.vue
@@ -1,21 +1,26 @@
 <template>
-  <div class="form-field" :class="`${tag} ${error ? 'error' : ''}`">
-    <SvgLoader
-      :url="`img/icons/${name ? name : 'default'}`"
-      v-if="type !== 'submit'"
-    />
-    <input
-      :type="type"
-      :id="id"
-      :name="name"
-      v-if="tag === 'input'"
-      :placeholder="placeholder"
-      @focus="focus"
-      @blur="blur"
-      v-model="inputData"
-    />
+  <div class="form-field">
+    <div :class="`${tag} ${error ? 'error' : ''} ${value ? 'focus' : ''}`">
+      <SvgLoader
+        :url="`img/icons/${name ? name : 'default'}`"
+        v-if="type !== 'submit'"
+      />
+      <input
+        :type="type"
+        :id="id"
+        :name="name"
+        v-if="tag === 'input'"
+        :placeholder="placeholder"
+        @click="focus"
+        @blur="blur"
+        v-model="inputData"
+        :pattern="regex"
+      />
 
-    <SvgLoader url="img/icons/cross" v-if="type !== 'submit'" />
+      <SvgLoader url="img/icons/cross" v-if="type !== 'submit'" />
+    </div>
+
+    <FieldInfo v-if="type != 'submit'" :type="name" />
 
     <Button v-if="type == 'submit'" :disabled="disabled" />
   </div>
@@ -73,94 +78,99 @@ export default {
 
 <style lang="scss" scoped>
 .form-field {
-  position: relative;
   max-width: rem(300px);
   width: rem(300px);
   display: flex;
-  justify-content: flex-start;
-  align-items: center;
+  flex-direction: column;
 
   &:not(:first-child) {
-    margin-top: rem(40px);
+    margin-top: rem(35px);
   }
 
-  &.input,
-  &.textarea {
-    min-height: rem(50px);
-    padding: 0 rem(15px);
+  > div {
+    position: relative;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
 
-    &:before {
-      content: '';
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      background-color: #fff;
-      border-radius: rem(30px);
-      opacity: 0.15;
-      transition: all 0.3s ease-in-out;
-      z-index: -1;
-    }
+    &.input,
+    &.textarea {
+      min-height: rem(50px);
+      padding: 0 rem(15px);
 
-    &.focus {
       &:before {
-        opacity: 0.2;
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: #fff;
+        border-radius: rem(30px);
+        opacity: 0.15;
         transition: all 0.3s ease-in-out;
+        z-index: -1;
+      }
+
+      &.focus {
+        &:before {
+          opacity: 0.2;
+          transition: all 0.3s ease-in-out;
+        }
       }
     }
-  }
 
-  &.error {
+    &.error {
+      /deep/ svg {
+        &.cross {
+          height: rem(15px);
+          opacity: 1;
+          transform: translateX(rem(50px));
+          transition: all 0.15s ease-in-out;
+        }
+
+        g path {
+          fill: $error;
+          transition: all 0.3s ease-in-out;
+        }
+      }
+    }
+
     /deep/ svg {
+      min-width: rem(17px);
+
       &.cross {
-        height: rem(15px);
-        opacity: 1;
-        transform: translateX(rem(50px));
+        opacity: 0;
+        transform: translateX(rem(0px));
         transition: all 0.15s ease-in-out;
       }
 
       g path {
-        fill: $error;
+        fill: #fff;
         transition: all 0.3s ease-in-out;
       }
     }
-  }
 
-  /deep/ svg {
-    min-width: rem(17px);
-
-    &.cross {
-      opacity: 0;
-      transform: translateX(rem(0px));
-      transition: all 0.15s ease-in-out;
-    }
-
-    g path {
-      fill: #fff;
-      transition: all 0.3s ease-in-out;
-    }
-  }
-
-  input {
-    width: 100%;
-    height: 100%;
-    margin-left: rem(10px);
-    -webkit-appearance: none;
-    background-color: transparent;
-    border: none;
-    outline: 0;
-    font-size: rem(16px);
-    font-weight: 400;
-    color: #fff;
-
-    &::placeholder {
-      position: relative;
-      top: rem(2px);
+    input {
+      width: 100%;
+      height: 100%;
+      margin-left: rem(10px);
+      -webkit-appearance: none;
+      background-color: transparent;
+      border: none;
+      outline: 0;
       font-size: rem(16px);
       font-weight: 400;
-      opacity: 0.7;
       color: #fff;
+
+      &::placeholder {
+        position: relative;
+        top: rem(2px);
+        font-size: rem(16px);
+        font-weight: 400;
+        opacity: 0.7;
+        color: #fff;
+      }
     }
   }
 }
---
+++ components/form/FieldInfo.vue
@@ -0,0 +1,32 @@
+<template>
+  <span class="info" v-if="message">{{ message }}</span>
+</template>
+
+<script>
+export default {
+  name: 'FieldInfo',
+  props: {
+    type: String,
+  },
+  computed: {
+    message() {
+      switch (this.type) {
+        case 'password':
+          return 'Le mot de passe doit contenir 8 caractères minimum, 1 majuscule, 1 minuscule et 1 chiffre'
+          break
+        default:
+          return ''
+          break
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.info {
+  margin: rem(10px 0 0 10px);
+  font-size: rem(11px);
+  color: #fff;
+}
+</style>
